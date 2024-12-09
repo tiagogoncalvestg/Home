430b089260422213f7455ffeb9e2ee5aa5021a76
Tiago <Tiago@DESKTOP-C9DTPU3>
2024-12-09 09:40:52 -0300

    responsividade mobile ajustada

--- js/login.js
+++ js/login.js
@@ -1,16 +1,16 @@
 let form = document.querySelector('#inputEmail');
 
-  // Redireciona automaticamente para o projeto
-  form.addEventListener('focus', function() {
-    redirect();
-  });
+//  Redireciona automaticamente para o projeto
+form.addEventListener('focus', function () {
+  redirect();
+});
 
-  async function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
+async function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-  async function redirect() {
-    await sleep(500);
+async function redirect() {
+  await sleep(500);
 
-    window.location.href = './project.html';
-  }
+  window.location.href = './project.html';
+}
